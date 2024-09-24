67f25154ee3f4927657de7637b52892e73f950fa
Patricia P <ppeinado@student.42madrid.com>
2024-09-24 23:04:37 +0200

    cambios

--- get_next_line.c
+++ get_next_line.c
@@ -1,39 +1,57 @@
+#include "get_next_line.h"
+
 char *read_fd(fd)
 {
 	char buf[BUFFER_SIZE + 1];
 	ssize_t n_bytes;
 	
 	n_bytes = read(fd, buf, BUFFER_SIZE);
-	if (n_bytes = -1)
+	if (n_bytes < 1)
 		return (NULL);
+
 	return (buf);
 }
 
-char *fill_static(char *buf)
+char *fill_static(char *buf, int fd)
 {
 	static char *stocked;
 
 	if (!stocked)
 		stocked = ft_strdup(buf);
-	else
-		stocked = ft_strjoin(stocked, buf)
 	if(!stocked)
 		return (NULL);
-	line = looking_for_line;
-	if (!line)
-		return(NULL);
+	while (!line)
+	{
+		line = looking_for_line(stocked);
+		if(!line)
+		{
+			buf = read_fd(fd);
+			aux = ft_strjoin(buf, stocked);
+			stocked = ft_strdup(aux);
+			free(aux);
+		}
+	}
+	return(line);
+
 }
 
-char *looking_for_line(char *stocked)
+char *looking_for_line(static char *stocked)
 {
 	char *position;
 	char *line;
+	int end_stk;
 	
 	line = NULL;
-	if(position = ft_strchr(stocked, "\n"))
+	position = ft_strchr(stocked, '\n');
+	end_stk = ft_strlen(stocked);
+	if(!position)
+		ft_strchr(stocked, '\0');
+	if(position)
 	{
 		line	= ft_substr(stocked, 0, position - stocked);
+		stocked = ft_substr(stocked, position - stocked, end_stk);
 	}
+
 	return(line);
 }
 
@@ -42,4 +60,11 @@ char *get_next_line(int fd)
 	if (!fd || fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	buf = read_fd(fd);
+	if (!buf)
+		return(NULL);
+	line = fill_static(buf, fd);
+	if(!line)
+		return(NULL);
+	return(line);
+
 }
--- get_next_line.h
+++ get_next_line.h
@@ -7,8 +7,14 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
-
-
+size_t ft_strlen(const char *s);
+char  *ft_strdup(char *s);
+char *ft_strchr(const char *s, int c);
+char    *ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char *read_fd(fd);
+char *fill_static(char *buf, int fd);
+char *looking_for_line(static char *stocked);
+char *get_next_line(int fd)
 
 # endif
