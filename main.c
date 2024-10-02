f39c616f9222c570010ee4b5fc85d5c350207ef4
Patricia P <ppeinado@student.42madrid.com>
2024-10-02 19:57:04 +0200

    sin leaks

--- main.c
+++ main.c
@@ -5,12 +5,16 @@ int main(void)
 {
 	int i;
 	int fd;
-	
-	i = 6;
-	fd = open("/Users/patricia/ppatroclo/get_next_line/41_with_nl.txt", O_RDONLY);
+	char *line;
+
+	line = NULL;	
+	i = 10;
+	fd = open("/mnt/c/Users/User/Desktop/42/get_next_line/41_with_nl.txt", O_RDONLY);
 	while (i >= 0)
 	{
-		printf("%s", get_next_line(fd));
+		line =  get_next_line(fd);
+		printf("%s", line);
+		free(line);
 		i--;
 	}
 }
